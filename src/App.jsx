1c989e9993cd3cad72aed69e6b4c8ae2d525866b
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-15 19:44:33 +0500

    configuring routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Features from "./pages/Features"
+import Pricing from "./pages/Pricing"
+import Resources from "./pages/Resources"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+
 function App() {
 
   return (
     <>
-      hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}  />
+          <Route path="/features" element={<Features/>} />
+          <Route path="/pricing" element={<Pricing/>} />
+          <Route path="/resources" element={<Resources/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

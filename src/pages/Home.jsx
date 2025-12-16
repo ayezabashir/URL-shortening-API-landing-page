959c7005d641dee9274cc798bbf92e4f1610bb32
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-16 21:09:08 +0500

    shorten form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import HeroComponent from '../components/HeroComponent'
+import ShortnerForm from '../components/ShortnerForm'
 
 const Home = () => {
   return (
     <>
       <HeroComponent />
+      <ShortnerForm />
     </>
   )
 }

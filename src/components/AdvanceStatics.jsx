3d925d22c1336af08258023f5f97c5b0bcbe9040
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-17 21:50:06 +0500

    statistics card component

--- src/components/AdvanceStatics.jsx
+++ src/components/AdvanceStatics.jsx
@@ -6,36 +6,39 @@ const stats = [
     {
         img: brandicon,
         title: "Brand Recognition",
-        description: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
+        description: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+        margin_top: "mt-15 lg:mt-0"
     },
     {
         img: detailedicon,
         title: "Detailed Records",
-        description: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
+        description: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+        margin_top: "mt-15 lg:mt-10"
     },
     {
         img: customicon,
-        title:"Fully Customizable",
-        description:"Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+        title: "Fully Customizable",
+        description: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+        margin_top: "mt-15 lg:mt-20"
     }
 ]
 
 const AdvanceStatics = () => {
-  return (
-    <section className="container">
-        <div className='flex flex-col justify-center items-center gap-5 mb-10'>
-            <h2 className='text-3xl text-black font-bold'>Advanced Statistics</h2>
-            <p className='text-gray font-semibold max-w-120 text-center'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
-        </div>
-        <div className='flex justify-between gap-5'>
-            {
-                stats.map((stat, index)=>(
-                    <StaticsCard key={index} stat_img={stat.img} stat_title={stat.title} stat_desc={stat.description} />
-                ))
-            }
-        </div>
-    </section>
-  )
+    return (
+        <section className="container">
+            <div className='flex flex-col justify-center items-center gap-5 mb-10'>
+                <h2 className='text-3xl text-black font-bold'>Advanced Statistics</h2>
+                <p className='text-gray font-semibold max-w-120 text-center'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+            </div>
+            <div className='flex flex-col lg:flex-row justify-between gap-5 mt-20'>
+                {
+                    stats.map((stat, index) => (
+                        <StaticsCard key={index} stat_img={stat.img} stat_title={stat.title} stat_desc={stat.description} margin_top={stat.margin_top} />
+                    ))
+                }
+            </div>
+        </section>
+    )
 }
 
 export default AdvanceStatics
--- src/components/StaticsCard.jsx
+++ src/components/StaticsCard.jsx
@@ -1,7 +1,7 @@
-const StaticsCard = ({ stat_img, stat_title, stat_desc }) => {
+const StaticsCard = ({ stat_img, stat_title, stat_desc, margin_top }) => {
     return (
-        <div className="bg-white w-1/3 relative p-6 rounded-md">
-            <div className="absolute -top-10 left-10 bg-purple rounded-full w-20 h-20 flex justify-center items-center">
+        <div className={`bg-white h-65 w-full lg:w-1/3 relative p-6 rounded-md ${margin_top}`}>
+            <div className="absolute -top-10 left-[50%] transform -translate-x-[50%] lg:transform-none lg:left-15 bg-purple rounded-full w-20 h-20 flex justify-center items-center">
                 <img className="object-cover" src={stat_img} alt={stat_title} />
             </div>
             <div className="mt-15">

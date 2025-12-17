a91a6acdd202c98ffbf2e6e5cb66e1130d1bb5d4
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-17 20:26:27 +0500

    links

--- src/components/OldLinks.jsx
+++ src/components/OldLinks.jsx
@@ -1,6 +1,12 @@
-const OldLinks = () => {
-    const handleCopy = () => {
+import { useState } from "react"
 
+const OldLinks = () => {
+    const [copyLink, setCopyLink] = useState(false);
+    const handleCopy = () => {
+        setCopyLink(true)
+        setTimeout(() => {
+            setCopyLink(false)
+        }, 1000);
     }
     return (
         <section className='container py-1'>
@@ -8,8 +14,10 @@ const OldLinks = () => {
                 <p className="text-black font-semibold wrap-anywhere w-full md:w-auto py-2.5 px-4 border-b-2 md:border-0 border-light-gray">https://frontendmentor.com</p>
                 <div className="py-2.5 px-4 flex flex-col md:flex-row justify-between items-center gap-10">
                     <p className="text-blue font-semibold wrap-anywhere">https://rel/k4uy</p>
-                    <button onClick={handleCopy} type='button' className='bg-blue hover:opacity-90 cursor-pointer text-white py-1.5 px-5 rounded-md font-semibold '>
-                        Copy
+                    <button onClick={handleCopy} type='button' className={`hover:opacity-90 cursor-pointer text-white py-1.5 px-5 rounded-md font-semibold ${copyLink ? "bg-purple" : "bg-blue"}`}>
+                        {
+                            copyLink ? "Copied!" : "Copy"
+                        }
                     </button>
                 </div>
             </div>

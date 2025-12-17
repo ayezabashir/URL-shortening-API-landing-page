622dfc6e599d216193a81b76402cb255eb325ccc
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-17 21:36:27 +0500

    statistics card

--- src/components/AdvanceStatics.jsx
+++ src/components/AdvanceStatics.jsx
@@ -1,35 +1,39 @@
 import StaticsCard from './StaticsCard'
-// import brandicon from "/icon-brand-recognition.svg";
-// import detailedicon from "/icon-detailed-records.svg";
-// import customicon from "/icon-fully-customizable.svg";
-// // const stats = [
-//     {
-//         img: brandicon,
-//         title: "Brand Recognition",
-//         description: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
-//     },
-//     {
-//         img: detailedicon,
-//         title: "Detailed Records",
-//         description: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
-//     },
-//     {
-//         img: customicon,
-//         title:"Fully Customizable",
-//         description:"Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
-//     }
-// ]
+import brandicon from "/icon-brand-recognition.svg";
+import detailedicon from "/icon-detailed-records.svg";
+import customicon from "/icon-fully-customizable.svg";
+const stats = [
+    {
+        img: brandicon,
+        title: "Brand Recognition",
+        description: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
+    },
+    {
+        img: detailedicon,
+        title: "Detailed Records",
+        description: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
+    },
+    {
+        img: customicon,
+        title:"Fully Customizable",
+        description:"Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+    }
+]
 
 const AdvanceStatics = () => {
   return (
     <section className="container">
-        <div className='text-center'>
-            <h2 className='text-3xl text-black'>Advanced Statistics</h2>
-            <p className='text-gray font-semibold'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+        <div className='flex flex-col justify-center items-center gap-5 mb-10'>
+            <h2 className='text-3xl text-black font-bold'>Advanced Statistics</h2>
+            <p className='text-gray font-semibold max-w-120 text-center'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
         </div>
-        <StaticsCard />
-        <StaticsCard />
-        <StaticsCard />
+        <div className='flex justify-between gap-5'>
+            {
+                stats.map((stat, index)=>(
+                    <StaticsCard key={index} stat_img={stat.img} stat_title={stat.title} stat_desc={stat.description} />
+                ))
+            }
+        </div>
     </section>
   )
 }
--- src/components/StaticsCard.jsx
+++ src/components/StaticsCard.jsx
@@ -1,9 +1,15 @@
-const StaticsCard = () => {
-  return (
-    <div>
-        Card 1
-    </div>
-  )
+const StaticsCard = ({ stat_img, stat_title, stat_desc }) => {
+    return (
+        <div className="bg-white w-1/3 relative p-6 rounded-md">
+            <div className="absolute -top-10 left-10 bg-purple rounded-full w-20 h-20 flex justify-center items-center">
+                <img className="object-cover" src={stat_img} alt={stat_title} />
+            </div>
+            <div className="mt-15">
+                <h3 className="font-bold text-black mb-4 text-xl">{stat_title}</h3>
+                <p className="text-gray text-sm leading-relaxed">{stat_desc}</p>
+            </div>
+        </div>
+    )
 }
 
 export default StaticsCard

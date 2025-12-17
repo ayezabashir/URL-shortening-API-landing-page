c515f7ee59f0701155313d09a8fa9755ec505f96
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-17 22:06:51 +0500

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import AdvanceStatics from '../components/AdvanceStatics'
 import HeroComponent from '../components/HeroComponent'
-import OldLinks from '../components/OldLinks'
 import ShortnerForm from '../components/ShortnerForm'
 
 const Home = () => {
@@ -10,6 +9,12 @@ const Home = () => {
       <div className='bg-light-gray relative'>
         <ShortnerForm />
         <AdvanceStatics />
+      </div>
+      <div className="w-full bg-purple bg-no-repeat bg-[url(/bg-boost-mobile.svg)] lg:bg-[url(/bg-boost-desktop.svg)]">
+        <div className="p-10 flex flex-col justify-center items-center">
+          <h4 className='text-white font-bold text-2xl mb-5'>Boost Your Link Today</h4>
+          <button className='bg-blue hover:opacity-90 cursor-pointer text-white py-2 px-5 rounded-3xl text-base font-bold'>Get Started</button>
+        </div>
       </div>
     </>
   )

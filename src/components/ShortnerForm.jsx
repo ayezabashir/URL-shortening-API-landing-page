63c1812be448a095e416573bec584c1243ea8201
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-20 20:45:42 +0500

    form update

--- src/components/ShortnerForm.jsx
+++ src/components/ShortnerForm.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react"
 import OldLinks from "./OldLinks"
 
 const ShortnerForm = () => {
+    const [url, setUrl] = useState("");
     return (
         <section className="container ">
             <div className="absolute -top-20 left-0 right-0 p-6">
                 <div className="mb-5 bg-purple bg-[url(/bg-shorten-mobile.svg)] md:bg-[url(/bg-shorten-desktop.svg)] rounded-xl">
                     <form className="p-10 flex flex-col gap-5 md:flex-row justify-center items-center" onSubmit={(e) => e.preventDefault()}>
-                        <input className="w-full md:w-[80%] bg-white py-3 px-5 rounded-xl outline-0" type="text" placeholder="Shorten a link here..." name="url_input" id="url_input" />
-                        <button className="w-full md:w-[20%] bg-blue hover:opacity-90 cursor-pointer text-white py-3 px-2 rounded-xl text-base font-bold" type="submit">Shorten It!</button>
+                        <input className="w-full md:flex-1 bg-white py-3 px-5 rounded-xl outline-0" type="text" placeholder="Shorten a link here..." onChange={(e)=>setUrl(e.target.value)} value={url} name="url_input" id="url_input" />
+                        <button className=" w-full md:w-auto bg-blue hover:opacity-90 cursor-pointer text-white py-3 px-5 rounded-xl text-base font-bold" type="submit">Shorten It!</button>
                     </form>
                 </div>
             </div>

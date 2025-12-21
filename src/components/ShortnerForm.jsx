d5fb0ee5fcd34f377c5a9072fdd0a055867f9901
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-21 22:24:00 +0500

    fetchnig data

--- src/components/ShortnerForm.jsx
+++ src/components/ShortnerForm.jsx
@@ -5,38 +5,31 @@ const ShortnerForm = () => {
     const [url, setUrl] = useState("");
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
-    const handleSubmit = async(e) => {
+    const apikey = ""
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (!url.trim()) {
             setError("Input feild is empty");
             return
         }
-        try{
+        try {
             setLoading(true);
             setError("");
-            
-            const response = await fetch("https://spoo.me/api/shorten", {
-                method: "POST",
+
+            const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`, {
                 headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({url:url.trim()})
-            })
+                    'Content-Type': 'application/json',
+                    'apikey': apikey
+                }
+            });
 
             const data = await response.json();
+            console.log(data.result.short_link);
 
-            if(data.error){
-                setError(data.error);
-                return;
-            }
-
-            setUrl(data.result_url)
-            console.log(data.result_url);
-
-        }catch(error){
+        } catch (error) {
             setError("Something went wrong. Please try again.");
             console.log(error)
-        }finally{
+        } finally {
             setLoading(false);
         }
     }
@@ -46,7 +39,7 @@ const ShortnerForm = () => {
                 <div className="mb-5 bg-purple bg-[url(/bg-shorten-mobile.svg)] md:bg-[url(/bg-shorten-desktop.svg)] rounded-xl">
                     <form className="p-10 flex flex-col gap-5 md:flex-row justify-center items-center" onSubmit={(e) => handleSubmit(e)}>
                         <input className="w-full md:flex-1 bg-white py-3 px-5 rounded-xl outline-0" type="text" placeholder="Shorten a link here..." onChange={(e) => setUrl(e.target.value)} value={url} name="url_input" id="url_input" />
-                        <button className={`w-full md:w-auto bg-blue hover:opacity-90 text-white py-3 px-5 rounded-xl text-base font-bold ${loading ? "cursor-not-allowed" : "cursor-pointer" }`} type="submit">Shorten It!</button>
+                        <button className={`w-full md:w-auto bg-blue hover:opacity-90 text-white py-3 px-5 rounded-xl text-base font-bold ${loading ? "cursor-not-allowed" : "cursor-pointer"}`} type="submit">Shorten It!</button>
                         {
                             error && <p id="error" className="absolute bottom-14 left-18 text-sm text-Red">{error}</p>
                         }

621b12df7c08f69b47773c3249c8f2040884ec5c
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-23 20:33:02 +0500

    features card

---
+++ src/components/FeaturesCard.jsx
@@ -0,0 +1,15 @@
+const FeaturesCard = ({icon, title,description}) => {
+  return (
+    <div className='flex flex-col justify-between items-center border border-blue py-10 px-5 rounded-xl min-h-70'>
+        <div className="border border-blue h-20 w-20 flex justify-center items-center group bg-blue rounded-full">
+            <p className="text-4xl transform group-hover:scale-110 ease-in-out duration-150 select-none">{icon}</p>
+        </div>
+        <div className='text-center mt-5'>
+            <h1 className='font-semibold text-xl text-purple'>{title}</h1>
+            <p className='text-base mt-2 text-black'>{description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturesCard
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import FeaturesCard from "../components/FeaturesCard";
+
+const allFeatures = [
+  {
+    icon: "⚡",
+    title: "Easy",
+    description:
+      "Easy and fast, enter the long link to get your shortened link",
+  },
+  {
+    icon: "✂️",
+    title: "Shortened",
+    description:
+      "Use any link, no matter what size, ShortURL always shortens",
+  },
+  {
+    icon: "🔒",
+    title: "Secure",
+    description:
+      "It is fast and secure, our service has data encryption",
+  },
+  {
+    icon: "📊",
+    title: "Statistics",
+    description:
+      "Check number of clicks that your shortened URL get",
+  },
+  {
+    icon: "🛡️",
+    title: "Reliable",
+    description:
+      "All links that try to spam and malware are deleted",
+  },
+  {
+    icon: "📱",
+    title: "Devices",
+    description:
+      "Compatible with smartphones, tablets and desktop",
+  },
+];
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className="container">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {
+          allFeatures.map((feature, index) => (
+            <FeaturesCard key={index} icon={feature.icon} title={feature.title} description={feature.description} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
 

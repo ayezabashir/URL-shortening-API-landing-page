6cef1e51e6a886fb784ae696b313226820dc7ead
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-23 14:59:28 +0500

    fetching links and copy functionality

--- src/components/SingleLink.jsx
+++ src/components/SingleLink.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 const SingleLink = ({ link }) => {
-
+    const [copied, setCopied] = useState(false);
+    const handleCopy = async () => {
+        await navigator.clipboard.writeText(link.short);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1500);
+    };
     return (
         <div className="bg-white rounded-md flex flex-col md:flex-row justify-between items-center">
             <p className="px-4 py-3 border-b md:border-0">
@@ -12,6 +17,13 @@ const SingleLink = ({ link }) => {
                     {link.short}
                 </p>
 
+                <button
+                    onClick={handleCopy}
+                    className={`px-5 py-2 rounded-md text-white ${copied ? "bg-purple" : "bg-blue"
+                        }`}
+                >
+                    {copied ? "Copied!" : "Copy"}
+                </button>
             </div>
         </div>
     );

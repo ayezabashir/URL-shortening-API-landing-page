026f40064cf6729d4b41be9b9ae3d70748773b80
Ayeza Bashir <ayezabashir46@gmail.com>
2025-12-23 20:12:49 +0500

    saving links to local storage

--- src/components/OldLinks.jsx
+++ src/components/OldLinks.jsx
@@ -1,34 +1,29 @@
 import { useState } from "react";
 const OldLinks = ({ links }) => {
+
     const [copiedLink, setCopiedLink] = useState(null);
-    const handleClick = async (shortLink) => {
-        await navigator.clipboard.writeText(shortLink);
-        setCopiedLink(shortLink); // mark this link as copied
-        setTimeout(() => setCopiedLink(null), 1500); // reset after 1.5s
-    };
+    const handleCopyToClipboard = async (link) =>{
+        await navigator.clipboard.writeText(link);
+        setCopiedLink(link);
+        setTimeout(() => {
+            setCopiedLink(null);
+        }, 1500);
+    }
     return (
         <div className="mt-6 space-y-3">
             {links.map((link, index) => (
                 <div
                     key={index}
-                    className=" bg-white flex justify-between bg-gray-800 p-3 rounded"
+                    className=" bg-white flex flex-col md:flex-row justify-between items-center bg-gray-800 p-3 rounded-lg"
                 >
-                    <p className="text-gray truncate font-semibold">{link.original}</p>
+                    <p className="text-gray truncate font-semibold" style={{textWrap: "auto"}}>{link.original}</p>
                     <div className="flex justify-between items-center gap-7">
-                        <a
-                            href={link.short}
-                            target="_blank"
-                            className="font-semibold text-blue hover:underline"
-                        >
+                        <p className="font-semibold text-blue wrap-anywhere">
                             {link.short}
-                        </a>
-                        <button
-                            className="bg-blue rounded-md font-bold text-white p-2 px-5"
-                            onClick={() => handleClick(link.short)}
-                        >
+                        </p>
+                        <button className="bg-blue rounded-md font-bold text-white p-2 px-5" onClick={() => handleCopyToClipboard(link.short)}>
                             {copiedLink === link.short ? "Copied!" : "Copy"}
                         </button>
-
                     </div>
                 </div>
             ))}

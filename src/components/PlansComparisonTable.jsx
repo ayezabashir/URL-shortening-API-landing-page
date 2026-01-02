f349e8e24f3700afe0230de916f91ef9dde06828
Ayeza Bashir <ayezabashir46@gmail.com>
2026-01-02 21:58:07 +0500

    plans comparisons

--- src/components/PlansComparisonTable.jsx
+++ src/components/PlansComparisonTable.jsx
@@ -1,35 +1,47 @@
 const PlansComparisonTable = ({ t_heading, t_features }) => {
     return (
-        <table className="my-5 border border-gray rounded-2xl">
-            <tbody>
-                <tr>
-                    <th>
-                        {t_heading}
-                    </th>
-                </tr>
-                {
-                    t_features.map((feature, index) => (
-                        <tr key={index} className="py-2">
-                            <td className="text-sm text-purple font-medium">
-                                {feature.name}
-                            </td>
-                            <td className="text-sm text-purple font-light">
-                                {feature.pro_Monthly}
-                            </td>
-                            <td className="text-sm text-purple font-light">
-                                {feature.premium_Monthly}
-                            </td>
-                            <td className="text-sm text-purple font-light">
-                                {feature.pro_Yearly}
-                            </td>
-                            <td className="text-sm text-purple font-light">
-                                {feature.premium_Yearly}
-                            </td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+        <div className="my-5 border border-gray rounded-2xl p-5 w-full overflow-x-auto">
+            <h4 className="w-full font-bold text-lg mb-5 ">
+                {t_heading}
+            </h4>
+            <table className="w-full">
+                <tbody>
+                    <tr>
+                        <th className="w-[25%] font-semibold pb-5">Name</th>
+                        <th className="font-semibold pb-5 whitespace-nowrap">Free</th>
+                        <th className="font-semibold pb-5 whitespace-nowrap">Pro Monthly</th>
+                        <th className="font-semibold pb-5 whitespace-nowrap">Premium Monthly</th>
+                        <th className="font-semibold pb-5 whitespace-nowrap">Pro Yearly</th>
+                        <th className="font-semibold pb-5 whitespace-nowrap">Premium Yearly</th>
+                    </tr>
+                    {
+                        t_features.map((feature, index) => {
+                            const isLast = index === t_features.length - 1;
+                            return <tr key={index} >
+                                <td className={`w-[25%] text-sm text-purple py-4 font-medium ${isLast ? "border-0" : "border-b border-gray"}`}>
+                                    {feature.name}
+                                </td>
+                                <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
+                                    {feature.free}
+                                </td>
+                                <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
+                                    {feature.pro_Monthly}
+                                </td>
+                                <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
+                                    {feature.premium_Monthly}
+                                </td>
+                                <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
+                                    {feature.pro_Yearly}
+                                </td>
+                                <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
+                                    {feature.premium_Yearly}
+                                </td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 

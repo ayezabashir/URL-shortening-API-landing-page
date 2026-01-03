74a928eb8c2ed37a5b6ab443aa6d04f13490c2d3
Ayeza Bashir <ayezabashir46@gmail.com>
2026-01-03 10:48:10 +0500

    plans comparison

--- src/components/PlansComparisonTable.jsx
+++ src/components/PlansComparisonTable.jsx
@@ -1,4 +1,10 @@
+import checkicon from "/circle-check.svg"
+import falseicon from "/circle-xmark.svg"
 const PlansComparisonTable = ({ t_heading, t_features }) => {
+    const iconMap = {
+        unincluded: falseicon,
+        included: checkicon
+    }
     return (
         <div className="my-5 border border-gray rounded-2xl p-5 w-full overflow-x-auto">
             <h4 className="w-full font-bold text-lg mb-5 ">
@@ -22,19 +28,29 @@ const PlansComparisonTable = ({ t_heading, t_features }) => {
                                     {feature.name}
                                 </td>
                                 <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
-                                    {feature.free}
+                                    {
+                                        feature.free === "unincluded" ? <img src={iconMap.unincluded} alt="unincluded" className="w-5" /> : feature.free
+                                    }
                                 </td>
                                 <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
-                                    {feature.pro_Monthly}
+                                    {
+                                        feature.pro_Monthly === "unincluded" ? <img src={iconMap.unincluded} alt="unincluded" className="w-5" /> : feature.pro_Monthly
+                                    }
                                 </td>
                                 <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
-                                    {feature.premium_Monthly}
+                                    {
+                                        feature.premium_Monthly === "unincluded" ? <img src={iconMap.unincluded} alt="unincluded" className="w-5" /> : feature.premium_Monthly
+                                    }
                                 </td>
                                 <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
-                                    {feature.pro_Yearly}
+                                    {
+                                        feature.pro_Yearly === "unincluded" ? <img src={iconMap.unincluded} alt="unincluded" className="w-5" /> : feature.pro_Yearly
+                                    }
                                 </td>
                                 <td className={`text-sm text-purple py-4 font-light ${isLast ? "border-0" : "border-b border-gray"}`}>
-                                    {feature.premium_Yearly}
+                                    {
+                                        feature.premium_Yearly === "unincluded" ? <img src={iconMap.unincluded} alt="unincluded" className="w-5" /> : feature.premium_Yearly
+                                    }
                                 </td>
                             </tr>
                         })
